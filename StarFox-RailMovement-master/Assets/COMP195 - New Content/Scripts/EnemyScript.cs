dc5c218f3996e89f690cb5e183b03f7bd9e7c9f7
Claryse Adams <c_adams18@u.pacific.edu>
2021-02-22 13:47:42 -0800

    Implemented enemy prefab that follows/aims at player

---
+++ StarFox-RailMovement-master/Assets/COMP195 - New Content/Scripts/EnemyScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+	public GameObject player;
+	public Transform target; 
+ 	public Transform projectile;
+ 
+ 	public float maximumLookDistance = 30;
+ 	public float maximumAttackDistance = 10;
+ 	public float minimumDistanceFromPlayer = 2;
+ 
+ 	public float rotationDamping = 2;
+ 
+ 	public float shotInterval = 0.5F;
+ 
+ 	private float shotTime = 0;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+       	target = player.transform;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+ 
+		if(distance <= maximumLookDistance)
+		{
+			LookAtTarget();
+		}   
+    }
+
+	void LookAtTarget()
+	{
+		Vector3 dir = target.position - transform.position;
+		//dir.y = 0;
+		Quaternion rotation = Quaternion.LookRotation(dir);
+		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationDamping);
+	}
+}
