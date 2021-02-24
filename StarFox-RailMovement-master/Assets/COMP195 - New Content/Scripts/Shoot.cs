e1bb0f41cf57ba609118168c2ca3d97306478fde
aalmanz1 <aralmanza16@gmail.com>
2021-02-23 17:07:32 -0800

    Implemented very basic shooting script

---
+++ StarFox-RailMovement-master/Assets/COMP195 - New Content/Scripts/Shoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    public KeyCode shoot_key = KeyCode.Space;
+    public GameObject projectile;
+    public float shoot_force;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(shoot_key))
+        {
+            GameObject shot = GameObject.Instantiate(projectile, transform.position, transform.rotation);
+            shot.GetComponent<Rigidbody>().AddForce(transform.forward * shoot_force);
+        }
+    }
+}
