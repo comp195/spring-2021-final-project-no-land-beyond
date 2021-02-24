02a9ce16f444abcf0d81bdf306ea9603484f7e11
aalmanz1 <aralmanza16@gmail.com>
2021-02-23 23:04:44 -0800

    Added a few shooting effects + scripts. Warning: CM vcam2 no longer working properly

--- StarFox-RailMovement-master/Assets/COMP195 - New Content/Scripts/Projectile.cs
+++ StarFox-RailMovement-master/Assets/COMP195 - New Content/Scripts/Projectile.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public GameObject impact;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,23 @@ public class Projectile : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point;
+
+        if (impact != null)
+        {
+            var impactVFX = Instantiate(impact, pos, rot);
+            var psImpact = impactVFX.GetComponent<ParticleSystem>();
+            if (psImpact != null)
+                Destroy(impactVFX, psImpact.main.duration);
+            else
+            {
+                var psChild = impactVFX.transform.GetChild(0).GetComponent<ParticleSystem>();
+                Destroy(impactVFX, psChild.main.duration);
+            }
+        }
+
         Destroy(gameObject);
     }
 }
--- StarFox-RailMovement-master/Assets/COMP195 - New Content/Scripts/Shoot.cs
+++ StarFox-RailMovement-master/Assets/COMP195 - New Content/Scripts/Shoot.cs
@@ -7,16 +7,20 @@ public class Shoot : MonoBehaviour
 
     public KeyCode shoot_key = KeyCode.Space;
     public GameObject projectile;
+    public GameObject muzzleflash;
     public float projectile_speed;
     public float fire_rate;
-    public int destroy_after = 2;
 
     private float time_to_fire = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //if (muzzleflash != null)
+        //{
+        //    var muzzleVFX = Instantiate(muzzleflash, transform.position, Quaternion.identity);
+        //    muzzleVFX.transform.forward = gameObject.transform.forward;
+        //}
     }
 
     // Update is called once per frame
@@ -26,8 +30,18 @@ public class Shoot : MonoBehaviour
         {
             time_to_fire = Time.time + 1 / fire_rate;
             GameObject shot = GameObject.Instantiate(projectile, transform.position, transform.rotation);
+            GameObject muzzle = GameObject.Instantiate(muzzleflash, transform.position, transform.rotation);
             shot.GetComponent<Rigidbody>().AddForce(transform.forward * projectile_speed);
-            Destroy(shot, destroy_after);
+            var psMuzzle = muzzle.GetComponent<ParticleSystem>();
+            if (psMuzzle!= null)
+            {
+                Destroy(muzzle, psMuzzle.main.duration);
+            }
+            else
+            {
+                var psChild = muzzle.transform.GetChild(0).GetComponent<ParticleSystem>();
+                Destroy(muzzle, psChild.main.duration);
+            }
         }
     }
 }
