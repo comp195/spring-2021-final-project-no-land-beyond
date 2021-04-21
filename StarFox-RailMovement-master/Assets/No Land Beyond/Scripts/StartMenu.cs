ee55b691670cdf2d8ebb55451455781cffb875d7
aalmanz1 <aralmanza16@gmail.com>
2021-04-20 21:51:57 -0700

    Added Main Menu

---
+++ StarFox-RailMovement-master/Assets/No Land Beyond/Scripts/StartMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class StartMenu : MonoBehaviour
+{
+    public void change_scene()
+    {
+        if (EventSystem.current.currentSelectedGameObject.name == "Play")
+        {
+            SceneManager.LoadScene("Level 1");
+        }
+    }
+}
