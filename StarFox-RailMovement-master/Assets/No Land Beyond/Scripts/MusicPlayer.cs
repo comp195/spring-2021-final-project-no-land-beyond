4641e6ea6e64a0c207e9f0ac655fc3a5f7aff60a
aalmanz1 <aralmanza16@gmail.com>
2021-04-28 13:22:23 -0700

    Soundtrack and enemy audio!

---
+++ StarFox-RailMovement-master/Assets/No Land Beyond/Scripts/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioClip[] clips;
+    private AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = FindObjectOfType<AudioSource>();
+        source.loop = false;
+    }
+
+    private AudioClip get_random_track()
+    {
+        return clips[Random.Range(0, clips.Length)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!source.isPlaying)
+        {
+            source.clip = get_random_track();
+            source.Play();
+        }
+    }
+}
