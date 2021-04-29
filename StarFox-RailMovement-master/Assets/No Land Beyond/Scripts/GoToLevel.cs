2870024de33e8a8c47fc3cb4ab09491cb499c8d7
aalmanz1 <aralmanza16@gmail.com>
2021-04-28 23:54:02 -0700

    Pushing tutorial level

---
+++ StarFox-RailMovement-master/Assets/No Land Beyond/Scripts/GoToLevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class GoToLevel : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] bool interact_tag;
+    [Header("Tag")]
+    [SerializeField] string tag;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (interact_tag && other.transform.CompareTag(tag))
+        {
+            SceneManager.LoadScene("Level 1");
+        }
+    }
+}
