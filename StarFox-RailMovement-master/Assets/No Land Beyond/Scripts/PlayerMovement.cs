f32998199e098b28201e31ce0da4188c8cc70111
aalmanz1 <aralmanza16@gmail.com>
2021-04-29 16:56:11 -0700

    Removed script from enemy prefabs, caused bug that would warp to level1

--- StarFox-RailMovement-master/Assets/No Land Beyond/Scripts/PlayerMovement.cs
+++ StarFox-RailMovement-master/Assets/No Land Beyond/Scripts/PlayerMovement.cs
@@ -39,10 +39,10 @@ public class PlayerMovement : MonoBehaviour
 
     private int upgrades = 0;
 
-    //public Text killsText; //displays enemies shot
-    //public float timePassed = 0.0f; //gives player two minutes
-    //public Text timerText; // used for showing time
-    //public Text healthText;
+    public Text killsText; //displays enemies shot
+    public float timePassed = 0.0f; //gives player two minutes
+    public Text timerText; // used for showing time
+    public Text healthText;
 
     private bool paused = false;
     public bool collidingWithPowerUp = false;
@@ -58,10 +58,10 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        //timePassed += Time.deltaTime;
-        //killsText.text = "Kills: " + enemiesShot; 
-        //healthText.text = "Health: " + health; 
-        //timerText.text = timePassed.ToString("0");
+        timePassed += Time.deltaTime;
+        killsText.text = "Kills: " + enemiesShot; 
+        healthText.text = "Health: " + health; 
+        timerText.text = timePassed.ToString("0");
 
 
         float h = joystick ? Input.GetAxis("Horizontal") : Input.GetAxis("Mouse X");
