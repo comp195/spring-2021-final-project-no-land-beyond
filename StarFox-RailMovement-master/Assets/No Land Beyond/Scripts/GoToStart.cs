95d9d5d21de47ed1060ea3cb7601e157e2d01c1a
aalmanz1 <aralmanza16@gmail.com>
2021-04-30 19:55:29 -0700

    End of level will now play a little animation and take player back to Start Menu

---
+++ StarFox-RailMovement-master/Assets/No Land Beyond/Scripts/GoToStart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class GoToStart : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] bool interact_tag;
+    [Header("Tag")]
+    [SerializeField] string tag;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (interact_tag && other.transform.CompareTag(tag))
+        {
+            SceneManager.LoadScene("Start Menu");
+        }
+    }
+}
+  
