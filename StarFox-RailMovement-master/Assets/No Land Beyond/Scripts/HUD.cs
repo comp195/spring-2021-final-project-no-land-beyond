fe9e9682cacf24145008a04b176216767833f9e1
Claryse Adams <c_adams18@u.pacific.edu>
2021-04-30 22:19:26 -0700

    end game

--- StarFox-RailMovement-master/Assets/No Land Beyond/Scripts/HUD.cs
+++ StarFox-RailMovement-master/Assets/No Land Beyond/Scripts/HUD.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class HUD : MonoBehaviour
 {
@@ -40,8 +41,25 @@ public class HUD : MonoBehaviour
 
     public void Die(){
         StartCoroutine("WaitForDie");
+        StartCoroutine(LoadStart());
         //Time.timeScale = 0;
         
+    }
+
+    IEnumerator LoadStart()
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Start Menu");
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
     }
 
     IEnumerator WaitForDie(){
